43c27da1bb2fe13afcfe62c3fe4827ad45dfaa5a
Yuliya Laptsinskaya <89513166+Kroshka0001@users.noreply.github.com>
2023-02-28 01:17:50 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
+function isInbracketsConfigStart(c, bracketsConfig) {
+    for (var j = 0 ; j < bracketsConfig.length; j++)
+        if(bracketsConfig[j][0] == c) return true
+    return false
+}
+
+function isInbracketsConfigEnd(c, bracketsConfig) {
+    for (var j = 0 ; j < bracketsConfig.length; j++)
+        if(bracketsConfig[j][1] == c) return true
+    return false
+}
+
+function isPpareBrackets(sSymbol,eSymbol, bracketsConfig) {
+    for (var j = 0 ; j < bracketsConfig.length; j++)
+        if((bracketsConfig[j][0] == sSymbol) && (bracketsConfig[j][1] == eSymbol)) return true
+    return false
+}
+
+function isSimpleBrackets(c,bracketsConfig) {
+    for (var j = 0 ; j < bracketsConfig.length; j++) {
+        if((bracketsConfig[j][0] == c) && (bracketsConfig[j][1] == c)) return true
+    }
+    return false
+}
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    var tmp = ''
+    for (var i = 0; i < str.length; i++) {
+        var currentSymbol = str.charAt(i)
+        var isSimpleB = isSimpleBrackets(currentSymbol, bracketsConfig)
+
+        if (currentSymbol == tmp.charAt(tmp.length-1) && isSimpleB){
+            if (isPpareBrackets(tmp.charAt(tmp.length-1), currentSymbol, bracketsConfig))
+                tmp = tmp.slice(0,tmp.length-1)
+
+        } else if (isInbracketsConfigStart(currentSymbol, bracketsConfig)) {
+            tmp = tmp + currentSymbol
+        } else if (isInbracketsConfigEnd(currentSymbol, bracketsConfig)) {
+            if ( isPpareBrackets(tmp.charAt(tmp.length-1), currentSymbol, bracketsConfig)) {
+                tmp = tmp.slice(0,tmp.length-1)
+            } else return false
+        }
+    }
+    if (tmp.length != 0 ) return false
+    return true
 }
